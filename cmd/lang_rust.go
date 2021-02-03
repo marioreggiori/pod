f988ef5df65b5a19416ab3987bccc0afc0510dec
Mario Reggiori <git@marioreggiori.com>
2021-02-03 12:08:23 +0100

    add rustdoc support

--- cmd/lang_rust.go
+++ cmd/lang_rust.go
@@ -6,5 +6,6 @@ func init() {
 	var opts = &utils.RunWithDockerOptions{Image: "rust"}
 
 	rootCmd.AddCommand(cmd("rustc", "Rust compiler", opts))
+	rootCmd.AddCommand(cmd("rustdoc", "Rust documentation", opts))
 	rootCmd.AddCommand(cmd("cargo", "Rust package manager", opts))
 }
